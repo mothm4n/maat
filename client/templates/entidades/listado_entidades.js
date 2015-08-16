2da88495f4d07547e470ec6064067fc51e15a81a
Marc Gelpi Mateu <mothm4n@MacBook-Pro-de-Marc.local>
2015-08-16 17:00:20 +0200

    first commit

---
+++ client/templates/entidades/listado_entidades.js
@@ -0,0 +1,28 @@
+/*[
+      {
+        nombre: 'leaf'
+      },
+      {
+        nombre: 'fire'
+      },
+      {
+        nombre: 'tint'
+      },
+      {
+        nombre: 'eye-open'
+      },
+      {
+        nombre: 'apple'
+      },
+      {
+        nombre: 'grain'
+      }
+    ]
+*/
+
+Template.listadoEntidades.helpers({
+  entidades: function(){
+    return entidades.find();
+  }
+});
+
---
+++ server/basetestBBDD.js
@@ -0,0 +1,68 @@
+if (entidades.find().count() === 0) {
+	entidades.insert({
+    "guid": "fe4ad982-8a40-4e0e-8c0f-17ba7fc2f38d",
+    "img": "http://placehold.it/124x124",
+    "texto": "Ut amet ipsum id nisi consectetur cillum eiusmod ipsum aliqua Lorem. Ea duis pariatur ad tempor sit cupidatat aliquip labore.",
+    "code": [
+      "pawn",
+      "knight",
+      "queen"
+    ],
+    "titulo": "Berger Hinton"
+  });
+	entidades.insert({
+    "guid": "77c15ba5-1845-40d3-8e0c-5f5c5d420b50",
+    "img": "http://placehold.it/124x124",
+    "texto": "Proident laborum fugiat duis exercitation culpa ea ut non nulla do consequat amet elit officia. Qui Lorem adipisicing sint proident commodo velit duis commodo eiusmod pariatur incididunt.",
+    "code": [
+      "bishop",
+      "knight",
+      "knight"
+    ],
+    "titulo": "Carey Leonard"
+  });
+	entidades.insert({
+    "guid": "f785bbae-404c-4b07-92d3-edbf87350a4f",
+    "img": "http://placehold.it/124x124",
+    "texto": "Dolore nisi duis officia ullamco reprehenderit minim esse non. Incididunt ex nostrud proident id anim fugiat veniam ullamco ut officia.",
+    "code": [
+      "bishop",
+      "bishop",
+      "pawn",
+      "pawn",
+      "tower"
+    ],
+    "titulo": "Beach Yang"
+  });
+	entidades.insert({
+    "guid": "9ed04ceb-365c-4c29-bee2-f8b33a66bc0e",
+    "img": "http://placehold.it/124x124",
+    "texto": "Culpa esse nostrud occaecat nulla exercitation qui commodo laborum ea do et. Voluptate labore ut non dolor laborum.",
+    "code": [
+      "bishop",
+      "knight",
+      "queen",
+      "bishop",
+      "knight",
+      "knight"
+    ],
+    "titulo": "Betsy Christian"
+  });
+	entidades.insert({
+    "guid": "2a79065e-316e-4087-9cd7-57dfac28224f",
+    "img": "http://placehold.it/124x124",
+    "texto": "Sit irure ex ullamco aute Lorem eu. Duis ad cupidatat culpa ullamco Lorem minim cillum.",
+    "code": [
+      "bishop"
+    ],
+    "titulo": "Fern Patton"
+  });
+	entidades.insert({
+    "guid": "a1efd6fe-f401-4b3a-a033-c8d4ba187eea",
+    "img": "http://placehold.it/124x124",
+    "texto": "Dolor reprehenderit Lorem sit fugiat irure cillum in sint incididunt ex nisi. Labore aute enim adipisicing eu nisi excepteur eu quis tempor eiusmod consequat esse qui.",
+    "code": [],
+    "titulo": "Staci Herman"
+  });
+
+};
