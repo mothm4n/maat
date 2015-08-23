e2c6d796983118fb08041ac80711337a2a67630d
Marc Gelpi Mateu <mothm4n@MacBook-Pro-de-Marc.local>
2015-08-23 22:52:11 +0200

    cuatro cosas

--- client/templates/entidades/entidad_submit.js
+++ client/templates/entidades/entidad_submit.js
@@ -1,10 +1,13 @@
 Template.entitySubmit.events({
 	'submit form': function (e) {
 		e.preventDefault();
+		var codigo =_.sample(["bishop","knight","queen","pawn","knight"], 4);
 
 		var entity = {
 			titulo: $(e.target).find('[name=titulo]').val(),
 			texto: $(e.target).find('[name=texto]').val(),
+			img: "http://placehold.it/124x124",
+			code: codigo
 		};
 		entity._id = entidades.insert(entity);
 		Router.go('entidad.show',entity);
--- client/templates/layout/layout.js
+++ client/templates/layout/layout.js
@@ -1,5 +1,13 @@
 Template.layout.helpers({
 	tp: function () {
 		return Session.get('tituloPagina');
-	}
+	},
+	gd: function guid() {
+		  function s4() {
+		    return Math.floor((1 + Math.random()) * 0x10000)
+		      .toString(16)
+		      .substring(3);
+		  }
+		  return s4();
+		}
 });
